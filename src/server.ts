74aedbebf543210b46f3ffc1a95f30eaa84731e7
SUPRØ <00youareworngtho00@gmail.com>
2026-03-22 03:20:46 +0530

    feat: add favicon

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "pages"));
-app.use(express.static(path.join(__dirname, "pages")));
+app.set("views", path.join(__dirname, "public", "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
