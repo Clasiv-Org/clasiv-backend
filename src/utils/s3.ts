605f0f909d89af66a292ad8607f3d971563042f3
SUPRØ <00youareworngtho00@gmail.com>
2026-05-03 17:22:51 +0530

    feat(s3): change upload method to putObject

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,22 +1,22 @@
-import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3 } from "@/config/s3";
 
 const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 if (!S3_BUCKET_NAME) throw new Error("Missing required env var: S3_BUCKET_NAME");
 
 
-export const generateUploadPresignedUrl = async (key: string) => {
-	const { url, fields } = await createPresignedPost(s3(), {
-		Bucket: S3_BUCKET_NAME!,
+export const generateUploadPresignedUrl = async (key: string, fileSize: number) => {
+	const command = new PutObjectCommand({
+		Bucket: S3_BUCKET_NAME,
 		Key: key,
-		Conditions: [
-			["content-length-range", 1, 1 * 1024 * 1024], // 1 byte to 10MB
-			["eq", "$Content-Type", "application/pdf"],
-		],
-		Fields: {
-			"Content-Type": "application/pdf",
-		},
-		Expires: 180,
+		ContentType: "application/pdf",
+        ContentLength: fileSize
 	});
-	return { url, fields };
+
+	const url = await getSignedUrl(s3(), 
+		command, 
+		{ expiresIn: 300 } // 5 minutes
+	);
+	return url;
 };
