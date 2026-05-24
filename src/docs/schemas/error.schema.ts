0d06a357ec04511f8873dbf4363cc430909dbfbb
SUPRØ <00youareworngtho00@gmail.com>
2026-05-25 03:04:55 +0530

    docs(schemas): add AppError schema

---
+++ src/docs/schemas/error.schema.ts
@@ -0,0 +1,14 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     AppError:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: An error occurred
+ *         statusCode:
+ *           type: number
+ *           example: 400
+ */
