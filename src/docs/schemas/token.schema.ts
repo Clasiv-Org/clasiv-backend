c4733597511552a9f705fa8a1a1104faa4fe9674
SUPRØ <00youareworngtho00@gmail.com>
2026-05-24 14:48:12 +0530

    feat(docs): add Token schema

---
+++ src/docs/schemas/token.schema.ts
@@ -0,0 +1,14 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Token:
+ *       type: object
+ *       properties:
+ *         accessToken:
+ *           type: string
+ *           example: 17oyhasjdn17820yams21dk289jsklmA9012UJ_OISMAKSD_ey
+ *         refreshToken:
+ *           type: string
+ *           example: 1892JIOASMD0=389OISAMC8903-HJ1_OISMAX0_ey
+ */
