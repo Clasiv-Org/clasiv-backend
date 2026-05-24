448e46a0a261322376c182e3e3f76c8488abefbf
SUPRØ <00youareworngtho00@gmail.com>
2026-05-24 14:46:47 +0530

    feat(docs): add UserSafe schema

---
+++ src/docs/schemas/user.schema.ts
@@ -0,0 +1,43 @@
+// REFERENCE: @/types/users.ts [UserSafe]
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UserSafe:
+ *       type: object
+ *       properties:
+ *         fullName:
+ *           type: string
+ *           example: John Doe
+ *         userName:
+ *           type: string
+ *           nullable: true
+ *           example: johndoe
+ *         emailId:
+ *           type: string
+ *           nullable: true
+ *           example: john@example.com
+ *         phoneNo:
+ *           type: string
+ *           nullable: true
+ *           example: "+919903645570"
+ *         baseRole:
+ *           type: number
+ *           example: 1
+ *         createdAt:
+ *           type: string
+ *           nullable: true
+ *           example: "2026-05-23T15:18:40.298Z"
+ *         modifiedAt:
+ *           type: string
+ *           nullable: true
+ *           example: "2026-05-23T15:18:40.298Z"
+ *         activatedAt:
+ *           type: string
+ *           nullable: true
+ *           example: null
+ *         lastLoginAt:
+ *           type: string
+ *           nullable: true
+ *           example: null
+ */
