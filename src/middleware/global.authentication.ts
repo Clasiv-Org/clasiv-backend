29a5e17e1126b830fcfb033f755e502ec62c88ea
SUPRØ <00youareworngtho00@gmail.com>
2026-05-31 10:58:06 +0530

    refactor(middleware): remove unecessary try-catch from global auth middleware

--- src/middleware/global.authentication.ts
+++ src/middleware/global.authentication.ts
@@ -6,20 +6,17 @@ import type {
 import { verifyAccessToken } from "@/utils/token";
 import { AppError } from "@/utils/error";
 
-const authentication = (req: Request, res: Response, next: NextFunction) => {
+const authentication = async (req: Request, _res: Response, next: NextFunction) => {
 	const authHeader = req.headers.authorization;
-	if(!authHeader) {
+
+	if(!authHeader || !authHeader.startsWith('Bearer ')) {
 		throw new AppError("No token", 401);
 	}
 
 	const token = authHeader?.split(" ")[1];
-	try {
-		const decode = verifyAccessToken(token!);
-		req.user = decode;
-        next();
-	} catch (error) {
-        throw new AppError("Invalid token", 401);
-	}
+	const decode = verifyAccessToken(token!);
+	req.user = decode;
+	next();
 }
 
 export default authentication;
